14f41f582103767f8ca7c8f3b5f8e1877e4f9290
fchrgrib <16521492@mahasiswa.itb.ac.id>
2023-02-26 00:30:41 +0700

    kapal

--- map.cpp
+++ map.cpp
@@ -13,6 +13,9 @@ map::map(int size) {
 void map::setObj(kapal x) {
     this->objMap[x.getX()+1][x.getY()+1]=x.getName();
 }
+void map::deleteObj(kapal x) {
+    this->objMap[x.getX()+1][x.getY()+1]=" ";
+}
 void map::setMap() {
     for(int i=0;i<this->size+2;i++){
         for(int j=0;j< this->size+2;j++){
--- map.h
+++ map.h
@@ -20,6 +20,7 @@ public:
 
     int getSize();
     void setObj(kapal x);
+    void deleteObj(kapal x);
     void setMap();
     void showMap();
 
